40cf8d6b5c871a788ff8e976fcc68411821ec369
mdfasano <mdfasano16@gmail.com>
2023-08-24 16:27:23 -0400

    set the board on load

--- js/CheckersPiece.js
+++ js/CheckersPiece.js
@@ -1,42 +1,23 @@
 // a class for all playing pieces used in a checkers game
 class CheckersPiece {
-    // takes a player signifier and 
-    // col/row indeced to represent location on game board
-    constructor () {
-        if (arguments.length >= 3) {
-            // expected case
-            this.player = arguments[0];
-            this.setLocation (arguments[1], arguments[2]);
-        } else if (arguments)
-        if (arguments.length === 0) {
-            // if no parameters passed, a new object is created with both
-            // location and player as null
-            this.player = null;
-            this.setLocation (null, null);
-        } else if (arguments.length === 1) {
-            // when one parameter is passed, it is treated as the 
-            // player indicator. The location is set to null
-            this.player = arguments[0];
-            this.setLocation (null, null)
-        } else if (arguments.length === 2) {
-            // when two parameters are passed, assumes that they represent
-            // col and row index (in that order) and sets 
-            // player indicator as null
-            this.setLocation (arguments[0], arguments[1]);
-            this.player = null;
-        }
+    // takes a turn number (1 or -1) 
+    // and a parent DOM element
+    constructor (turn, parentEl) {
+        this.player = turn;
+        this.parentEl = parentEl;
+        parentEl.domElement.innerHTML = this.imgLookup[this.player];
     }
 
 /* ---- stored variables --- */
     // represents the tile in game board at which this object exists 
-    location = {};
+    loc = {};
 
     // array holding locations that can be accessed from current location
     movableLocations = [];
     // uses the p1/p2 values as keys for images
     imgLookup = {
-        1: "../images/checkerspiece.png",
-        "-1": "../images/checkerspiece.png" 
+        1: '<img class="checkers-piece" src="../images/checkerspiece.png">',
+        "-1": '<img class="checkers-piece" src="../images/checkerspiecedark.jpg">'
     }
 
 /* ---- public class functions ----- */
@@ -48,15 +29,15 @@ class CheckersPiece {
     // only called by class
     whereCanIMove () {
         this.movableLocations = []; // clear array
-        this.movableLocations.push (this.location.colIdx + 1, this.location.rowIdx + this.player);
-        this.movableLocations.push (this.location.colIdx - 1, this.location.rowIdx + this.player);
+        this.movableLocations.push (this.loc.colIdx + 1, this.loc.rowIdx + this.player);
+        this.movableLocations.push (this.loc.colIdx - 1, this.loc.rowIdx + this.player);
     }
     //render function
 
     /* ----- private helper functions -----*/
     setLocation (colIdx, rowIdx) {
-        this.location.colIdx = colIdx;
-        this.location.rowIdx = rowIdx;
+        this.loc.colIdx = colIdx;
+        this.loc.rowIdx = rowIdx;
     }
 }
 
